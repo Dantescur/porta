e2ece9f3c3630794c6d0b8f9026ac514fc832ee7
Cesar Daniel <cesardaniel.9611@gmail.com>
2025-01-29 19:37:29 -0500

    finish markup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,24 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
-// import node from "@astrojs/node";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
+// import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "http://brawny-range.surge.sh/",
   integrations: [
     tailwind(),
     mdx(),
     icon({
       iconDir: "./src/icons",
     }),
+    sitemap(),
+    (await import("@playform/compress")).default(),
   ],
-
-  // adapter: node({
-  //   mode: "standalone",
-  // }),
 });
+
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter Variable", "sans-serif"],
+        mono: ["Fira Code"],
+      },
       colors: {
         background: "rgb(var(--color-background) / 1)",
         text: "rgb(var(--color-text) / 1)",
