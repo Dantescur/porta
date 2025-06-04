91f90a9455fce6442e2790bff938044e62f3732b
Cesar Daniel <cesardaniel.9611@gmail.com>
2025-06-03 23:48:20 -0400

    chore(forgot): i don't remember

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,19 +9,14 @@ import icon from "astro-icon";
 
 import sitemap from "@astrojs/sitemap";
 
+import vue from "@astrojs/vue";
+
 // import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://brawny-range.surge.sh/",
-  integrations: [
-    tailwind(),
-    mdx(),
-    icon({
-      iconDir: "./src/icons",
-    }),
-    sitemap(),
-    (await import("@playform/compress")).default(),
-  ],
+  integrations: [tailwind(), mdx(), icon({
+    iconDir: "./src/icons",
+  }), sitemap(), (await import("@playform/compress")).default(), vue()],
 });
-
